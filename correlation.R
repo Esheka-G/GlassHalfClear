09f0d318608a17cb8ec13f8a41792393c986753c
choihelenh <helen.choi3267@gmail.com>
2025-03-18 15:42:36 -0400

    norm

--- correlation.R
+++ correlation.R
@@ -1,8 +1,12 @@
+Secchi50 <- Secchi50 |>
+  mutate(Normalized_Secchi = (Secchi_m - min(Secchi_m, na.rm = TRUE)) /
+         (max(Secchi_m, na.rm = TRUE) - min(Secchi_m, na.rm = TRUE)))
+
 Merge <- merge(Secchi50, CTD, by = "DateTime")
 
 
 # Correlation of Secchi Depth and Turbity
-x <- Merge$Secchi_m
+x <- Merge$Normalized_Secchi
 y <- Merge$Turbidity_NTU
 
 correlation <- cor(x, y, use = "complete.obs")
