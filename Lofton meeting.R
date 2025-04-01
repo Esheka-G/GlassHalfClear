294b92e0d20e738c4b108cdc8f26e5c5c6a4cff9
choihelenh <helen.choi3267@gmail.com>
2025-04-01 14:44:01 -0400

    cleaning up messy data

--- Lofton meeting.R
+++ Lofton meeting.R
@@ -2,35 +2,33 @@ url <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_
 
 url_inflow <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-inflow-targets.csv.gz"
 
+site_list <- read_csv("https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv", show_col_types = FALSE)
+
 library(tidyverse)
 targets <- read_csv(url, show_col_types = FALSE)
 inflow_targets <- read_csv(url_inflow, show_col_types = FALSE)
 
+# Combining all variables together
 targets <- dplyr::bind_rows(targets, inflow_targets)
-
-site_list <- read_csv("https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv", show_col_types = FALSE)
-
-
-url2 <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-inflow-targets.csv.gz"
-inflow_targets <- read_csv(url2, show_col_types = FALSE)
-
-# Combine all data
-all_targets <- bind_rows(targets, inflow_targets)
-
-# Filter to Secchi observations only
-secchi_targets <- all_targets |>
-  filter(variable == "Secchi_m_sample")
-
-# Join with site metadata
-secchi_with_site_info <- secchi_targets |>
-  left_join(site_list, by = c("site_id"))
 
 #Split by reservoir
 # Falling Creek only
-fcre_secchi <- secchi_with_site_info %>%
+fcreData <- targets |>
   filter(site_id == "fcre")
 
 # Beaverdam only
-bvre_secchi <- secchi_with_site_info %>%
+bvreData <- targets |>
   filter(site_id == "bvre")
+
+
+# Find unique variables in variable column
+unique(fcreData$variable)
+
+unique(bvreData$variable)
+
+# "RDOTemp_C_6" & "RDOTemp_C_13"
+# These are only in bvre
+# daily mean oxygen in milligrams per liter from EXO;
+# focal depths bvre = 1.5 m, fcre = 1.6 m, measured using EXO and RDO
+
 
