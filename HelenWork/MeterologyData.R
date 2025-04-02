168091a1d4336673a937a4a1fa55c82d8f2890f7
choihelenh <helen.choi3267@gmail.com>
2025-04-02 13:01:39 -0400

    air temp data

--- HelenWork/MeterologyData.R
+++ HelenWork/MeterologyData.R
@@ -1,3 +1,9 @@
+library(tidyverse)
+
 url3 <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-met-targets.csv.gz"
 met_targets <- read_csv(url3, show_col_types = FALSE)
-#glimpse(met_targets)
+
+meterology_AirTemp <- met_targets |>
+  filter(variable == "AirTemp_C_mean") |>
+  rename(AirTemp_C_mean = observation) |>
+  select(-variable)
