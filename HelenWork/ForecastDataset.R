4a719f8ff1b8833d5ba2d1deba947130b0990fc0
choihelenh <helen.choi3267@gmail.com>
2025-04-02 12:51:04 -0400

    final combined dataset

--- HelenWork/ForecastDataset.R
+++ HelenWork/ForecastDataset.R
@@ -39,16 +39,25 @@ fcreData <- targets |>
 
 # Isolating fcre variables
 fcreData_Secchi <- fcreData |>
-  filter(variable == "Secchi_m_sample")
+  filter(variable == "Secchi_m_sample") |>
+  rename(Secchi_m = observation) |>
+  select(-variable)
 
 fcreData_WaterTemp <- fcreData |>
-  filter(variable == "Temp_C_mean")
+  filter(variable == "Temp_C_mean") |>
+  filter(depth_m == 1.6) |>
+  rename(Temp_C_mean = observation) |>
+  select(-variable)
 
 fcreData_ChloroA <- fcreData |>
-  filter(variable == "Chla_ugL_mean")
+  filter(variable == "Chla_ugL_mean") |>
+  rename(Chla_ugL_mean = observation) |>
+  select(-variable)
 
 fcreData_Bloom <- fcreData |>
-  filter(variable == "Bloom_binary_mean")
+  filter(variable == "Bloom_binary_mean") |>
+  rename(Bloom_binary_mean = observation) |>
+  select(-variable)
 
 # Beaverdam only
 bvreData <- targets |>
@@ -56,15 +65,58 @@ bvreData <- targets |>
 
 # Isolating bvre variables
 bvreData_Secchi <- bvreData |>
-  filter(variable == "Secchi_m_sample")
+  filter(variable == "Secchi_m_sample") |>
+  rename(Secchi_m = observation) |>
+  select(-variable)
 
 bvreData_WaterTemp <- bvreData |>
-  filter(variable == "Temp_C_mean")
+  filter(variable == "Temp_C_mean") |>
+  filter(depth_m == 1.5) |>
+  rename(Temp_C_mean = observation) |>
+  select(-variable)
 
 bvreData_ChloroA <- bvreData |>
-  filter(variable == "Chla_ugL_mean")
+  filter(variable == "Chla_ugL_mean") |>
+  rename(Chla_ugL_mean = observation) |>
+  select(-variable)
 
 bvreData_Bloom <- bvreData |>
-  filter(variable == "Bloom_binary_mean")
+  filter(variable == "Bloom_binary_mean") |>
+  rename(Bloom_binary_mean = observation) |>
+  select(-variable)
+
+# Combine Datasets
+
+fcre_Combined <- fcreData_Secchi |>
+  left_join(fcreData_WaterTemp |>
+              select(datetime, site_id, depth_m, Temp_C_mean),
+            by = c("datetime", "site_id")) |>
+  left_join(fcreData_ChloroA |>
+              select(datetime, site_id, Chla_ugL_mean),
+            by = c("datetime", "site_id")) |>
+  left_join(fcreData_Bloom |>
+              select(datetime, site_id, Bloom_binary_mean),
+            by = c("datetime", "site_id")) |>
+  select(-starts_with("depth_m")) |>
+  mutate(depth_m = 1.6) |>
+  relocate(depth_m, .after = Secchi_m)
+
+bvre_Combined <- bvreData_Secchi |>
+  left_join(bvreData_WaterTemp |>
+              select(datetime, site_id, depth_m, Temp_C_mean),
+            by = c("datetime", "site_id")) |>
+  left_join(bvreData_ChloroA |>
+              select(datetime, site_id, Chla_ugL_mean),
+            by = c("datetime", "site_id")) |>
+  left_join(bvreData_Bloom |>
+              select(datetime, site_id, Bloom_binary_mean),
+            by = c("datetime", "site_id")) |>
+  select(-starts_with("depth_m")) |>
+  mutate(depth_m = 1.5) |>
+  relocate(depth_m, .after = Secchi_m)
+
+
+
+
 
 
