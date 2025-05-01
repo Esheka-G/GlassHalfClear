53d6969e5a565ace25e174fab591784177581ad7
Esheka-G <eshekag@vt.edu>
2025-05-01 14:39:26 -0400

    pulling forecast starting from yesterday and getting 35 days ahead

--- EshekaWork/future_temp.R
+++ EshekaWork/future_temp.R
@@ -1,5 +1,9 @@
 remotes::install_github("ltreb-reservoirs/vera4castHelpers")
-weather <- vera4castHelpers::noaa_stage2(start_date = "2025-04-30")
+library(dplyr)
+
+start_forecast_date <- format(Sys.Date() - 1, "%Y-%m-%d")
+
+weather <- vera4castHelpers::noaa_stage2(start_date = start_forecast_date)
 df_future <- weather |>
   dplyr::filter(site_id == "fcre") |>
   dplyr::collect()
