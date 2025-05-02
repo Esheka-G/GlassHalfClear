8919dadb86c0e2f404cc50797c9acb424c84a8c8
Esheka-G <eshekag@vt.edu>
2025-05-02 16:21:41 -0400

    future airtemp @noon

--- EshekaWork/future_temp.R
+++ EshekaWork/future_temp.R
@@ -7,3 +7,18 @@ weather <- vera4castHelpers::noaa_stage2(start_date = start_forecast_date)
 df_future <- weather |>
   dplyr::filter(site_id == "fcre") |>
   dplyr::collect()
+
+library(lubridate)   # hour(), minute()
+
+airtemp_noon <- df_future %>%
+  ## 1. keep only the air‑temperature rows
+  filter(variable == "air_temperature") %>%
+
+  ## 2. keep only the rows whose timestamp is exactly 12:00:00
+  filter(hour(datetime) == 12, minute(datetime) == 0) %>%
+
+  ## 3. drop any forecast metadata you don’t need
+  mutate(date = as.Date(datetime)) %>%
+  select(date, air_temp = prediction)                # <-- change “prediction”
+#     to the actual value
+#     column in your data
