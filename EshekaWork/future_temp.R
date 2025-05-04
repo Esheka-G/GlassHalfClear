b42f900094db056e57d564084b25236d4a33a456
Esheka-G <eshekag@vt.edu>
2025-05-04 14:14:55 -0400

    pulling future precip data

--- EshekaWork/future_temp.R
+++ EshekaWork/future_temp.R
@@ -11,17 +11,32 @@ df_future <- weather |>
 library(lubridate)   # hour(), minute()
 
 
+agg_noon <- function(.data, value_col, fun = mean) {
+  .data %>%
+    filter(hour(datetime) == 12, minute(datetime) == 0) %>%
+    mutate(date = as.Date(datetime)) %>%
+    group_by(date) %>%
+    summarise(value = fun({{ value_col }}, na.rm = TRUE), .groups = "drop")
+}
+
 airtemp_noon <- df_future %>%
-  filter(variable == "air_temperature") %>%          # keep only air‑temp rows
-  filter(hour(datetime) == 12, minute(datetime) == 0) %>%   # keep 12:00:00
+  filter(variable == "air_temperature") %>%
+  mutate(pred_C = prediction - 273.15) %>%
+  agg_noon(pred_C) %>%
+  rename(air_temp_C = value)
+
+# 1. extract noon precipitation‑flux rows once per date
+precip_noon <- df_future %>%
+  filter(variable == "precipitation_flux",
+         hour(datetime) == 12, minute(datetime) == 0) %>%
   mutate(date        = as.Date(datetime),
-         air_temp_K  = prediction,        # <‑‑ value column in Kelvin
-         air_temp_C  = air_temp_K - 273.15) %>%      # convert to °C
-  select(date, air_temp_C)                           # keep what you need
+         flux        = prediction,          # kg m^-2 s^-1
+         precip_mm = flux * 86400) %>%
+  group_by(date) %>%
+  slice(1) %>%
+  ungroup() %>%
+  select(date, precip_mm)
 
-# result:
-#   date       air_temp_C
-#  2025-05-01     19.57
-#  2025-05-02     21.13
-#  ...
-
+# 2. join to the noon‑air‑temp table you already built
+airtemp_noon <- airtemp_noon %>%
+  left_join(precip_noon, by = "date")
